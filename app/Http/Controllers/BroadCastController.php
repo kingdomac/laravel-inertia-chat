892f68e66028665bfde2f5d2bed2a5b983b8427a
mohamad cheaib <mac.cheaib@gmail.com>
2023-03-11 20:47:22 +0200

    added validation to Broadcast

--- app/Http/Controllers/BroadCastController.php
+++ app/Http/Controllers/BroadCastController.php
@@ -5,17 +5,27 @@ namespace App\Http\Controllers;
 use App\Models\Message;
 use Illuminate\Http\Request;
 use App\Events\ChatRoomEvent;
+use Illuminate\Validation\Rule;
 use App\Events\PrivateMessageEvent;
 
 class BroadCastController extends Controller
 {
     public function broadcastToRoom()
     {
+        request()->validate([
+            'message' => ['required', 'string']
+        ]);
+
         ChatRoomEvent::dispatch(request('message'));
     }
 
     public function broadcastOneToOneMsg()
     {
+        request()->validate([
+            'user' => ['required', 'integer', Rule::exists('users', 'id')],
+            'message' => ['required', 'string']
+        ]);
+
         Message::create([
             'from_u' => auth()->id(),
             'to_u' => request('user'),
